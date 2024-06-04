5ee6615d9e2083dde192f1013752386fcedbaa65
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2024-06-04 23:05:47 +0200

    user signin added

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -2,6 +2,8 @@ import * as usersService from '../services/usersServices.js';
 import ctrlWrapper from '../decorators/ctrlWrapper.js';
 import HttpError from '../helpers/HttpError.js';
 import gravatar from 'gravatar';
+import compareHash from '../helpers/compareHash.js';
+import { createToken } from '../helpers/jwt.js';
 
 const signUp = async (req, res, next) => {
   try {
@@ -33,7 +35,43 @@ const signUp = async (req, res, next) => {
   }
 };
 
-const signIn = async (req, res) => {};
+const signIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await usersService.findUser({ email });
+
+    if (user.token) {
+      throw HttpError(403, 'User already logged in');
+    }
+
+    if (!user) {
+      throw HttpError(401, 'Email or password invalid');
+    }
+    const comparePass = await compareHash(password, user.password);
+
+    if (!comparePass) {
+      throw HttpError(401, 'Email or password invalid');
+    }
+
+    const { _id: id } = user;
+    const payload = {
+      id,
+    };
+
+    const token = createToken(payload);
+    await usersService.updateUser({ _id: id }, { token });
+
+    res.json({
+      token,
+      user: {
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const signOut = async (req, res) => {};
 
